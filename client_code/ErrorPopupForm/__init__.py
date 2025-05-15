8b0e617a8ba5dcb8145d9fe801f90973843dfe73
suchandan.mondal@edgewrapper.com <suchandan.mondal@edgewrapper.com>
2025-05-15 07:36:57 +0000

    Created ErrorPopupForm.RowTemplate1 and ErrorPopupForm; Edited settings and Form1

---
+++ client_code/ErrorPopupForm/__init__.py
@@ -0,0 +1,19 @@
+from ._anvil_designer import ErrorPopupFormTemplate
+from anvil import *
+
+
+class ErrorPopupForm(ErrorPopupFormTemplate):
+  def __init__(self, error_items=None, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    self.data_grid_1.columns = [
+      {"id": "display_name", "title": "Display Name", "data_key": "display_name", "width":150},
+      {"id": "app_name", "title": "App Name", "data_key": "app_name", "width":150},
+      {"id": "document_id", "title": "Document ID", "data_key": "document_id", "width":150},
+      {"id": "log_time", "title": "Log Time", "data_key": "log_time", "width":120},
+      {"id": "status_code", "title": "Status Code", "data_key": "status_code", "width":50},
+      {"id": "error_text", "title": "Error Text", "data_key": "error_text", "width":150}
+    ]
+    self.repeating_panel_1.items = error_items
+    
--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,88 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+from ..ErrorPopupForm import ErrorPopupForm
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    
 
-    # Any code you write here will run before the form opens.
+  def button_1_click(self, **event_args):
+    data = [
+      {"display_name":"Orders", "app_name":"nsOrders", "document_id":"2035884", "log_time":"2025-05-14", "status_code":"404", "error_text":"error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+      {"display_name":"Refunds", "app_name":"nsRefunds", "document_id":"2035885", "log_time":"2025-05-15", "status_code":"404", "error_text":"error:error"},
+    ]
+    alert_form = ErrorPopupForm(data)
+    alert(alert_form, large=True, title="Error Table")
